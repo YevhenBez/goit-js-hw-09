f252106b6892993685eb0055a95e747efeed3f6c
YevhenBez <byxgalter@bigmir.net>
2023-03-11 23:34:32 +0200

    Данными командами я ввёл в JS     body   (для последующего изменения его фона согласно ДЗ) и кнопки Старт и Стоп  Модуль 9 Задание 1
    Данными командами я ввёл в JS     body   (для последующего изменения его фона согласно ДЗ) и кнопки Старт и Стоп.  Модуль 9 Задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const body = document.querySelector("body");
 
+const pGoBack = body.firstElementChild;
+
+const btnStart = pGoBack.nextElementSibling;
+
+const btnStop = btnStart.nextElementSibling;
