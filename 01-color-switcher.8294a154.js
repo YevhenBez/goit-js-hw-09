7efbd22ac0945aff980968bd2c1d876f4704b1e4
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-03-11 21:35:31 +0000

    Deploying to gh-pages from @ YevhenBez/goit-js-hw-09@f252106b6892993685eb0055a95e747efeed3f6c 🚀

---
+++ 01-color-switcher.8294a154.js
@@ -0,0 +1,2 @@
+document.querySelector("body").firstElementChild.nextElementSibling.nextElementSibling;
+//# sourceMappingURL=01-color-switcher.8294a154.js.map
---
+++ 01-color-switcher.daa482e2.js
@@ -0,0 +1,2 @@
+document.querySelector("body").firstElementChild.nextElementSibling.nextElementSibling;
+//# sourceMappingURL=01-color-switcher.daa482e2.js.map
