adb6d0af44402140b70683332cb23dce1f9df2f7
YevhenBez <byxgalter@bigmir.net>
2023-03-12 19:45:41 +0200

    Данным куском кода я сделал так, что при нажатии на кнопку Start         backgroundColor     body     будет изменяться через каждую Одну секунду. И кнопка Start становится не активной. Задание 1 Модуль 9
    Данным куском кода я сделал так, что при нажатии на кнопку Start         backgroundColor     body     будет изменяться (для этого я использовал функцию     function getRandomHexColor() {
      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
    };         которая генерит случайный цвет) через каждую Одну секунду    (для этого я использовал            timerId = setInterval(() => {
        body.style.backgroundColor = getRandomHexColor();
    }, 1000);       ) . И кнопка Start становится не активной (для этого я использовал    btnStart.disabled = !btnStart.disabled;      ). Задание 1 Модуль 9. На остальные изменения можешь не смотреть. Т.к. я пытался использовать библиотеку     lodash.throttle     , но, как оказалось, для данного задания она пока не нужна.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,3 +5,16 @@ const pGoBack = body.firstElementChild;
 const btnStart = pGoBack.nextElementSibling;
 
 const btnStop = btnStart.nextElementSibling;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let timerId = null;
+
+btnStart.addEventListener("click", event => {
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+    btnStart.disabled = !btnStart.disabled;
+});
