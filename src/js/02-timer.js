f84a3946f17dc92df7354e32500335ba1f99907f
YevhenBez <byxgalter@bigmir.net>
2023-03-13 13:07:31 +0200

    Нашёл кнопку и сделал её не активной в начале запуска кода согласно ТЗ Задания 2 Модуля 9
    Нашёл кнопку и сделал её не активной (при помощи команды     btnStart.disabled = !btnStart.disabled;      )  в начале запуска кода согласно ТЗ Задания 2 Модуля 9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,12 +3,16 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
+const btnStart = document.querySelector('button[data-start]');
+
+btnStart.disabled = !btnStart.disabled;
+
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
     console.log(selectedDates[0]);
-  },
+    },
 };
