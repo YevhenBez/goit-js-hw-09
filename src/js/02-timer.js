c813c14808ca193079f3d402d541d71d48e0baf8
YevhenBez <byxgalter@bigmir.net>
2023-03-14 01:33:45 +0200

    Написал проверку даты выбранной и даты текущей Задание 2 Модуля 9
    Написал проверку даты выбранной    (         selectedDates[0]    )      и даты текущей (      options.defaultDate       ). Если дата выбранная меньше текущей, то тогда выводится сообщение при помощи команды      window.alert()   Задание 2 Модуля 9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,17 +9,26 @@ const inputField = document.querySelector('#datetime-picker');
 
 btnStart.disabled = !btnStart.disabled;
 
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-    console.log(selectedDates[0]);
+        console.log(selectedDates[0]);
+
+        if (selectedDates[0] <= options.defaultDate) {
+            window.alert("Please choose a date in the future");
+        } else {
+            btnStart.disabled = !btnStart.disabled;
+        }
     },
 };
 
+
 flatpickr(inputField, options);
+
 
 
 
