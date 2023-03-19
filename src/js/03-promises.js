1ebd88e8412624a7fa099f70f5baa114b40f9089
YevhenBez <byxgalter@bigmir.net>
2023-03-19 21:47:54 +0200

    Куском кода с 15 строки по 23 строку включительно я взял введённые пользователем значения с полей     First delay     ,       Delay step   ,     Amount       и записал их в     localStorage     Задание 3 Модуля 9
    Куском кода с 15 строки по 23 строку включительно я взял введённые пользователем значения с полей     First delay     ,       Delay step   ,     Amount       и записал их в     localStorage  . Остальной введённый новый код пока сырой   Задание 3 Модуля 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,13 +8,52 @@ const btn = form.lastElementChild;
 
 const inputAmount = btn.previousElementSibling.firstElementChild;
 
-console.log(inputAmount);
+let counter = 0;
+
+let amountFromUser;
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  // event.preventDefault();
+  
+  const delayStepAmount = {delay: inputDelay.value, step: inputStep.value, amount: inputAmount.value};
+
+  localStorage.setItem("data-from-user", JSON.stringify(delayStepAmount)); 
+}
+
+const delayYxxxx = 300;
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  
+  while (counter < 10) {
+  console.log("counter: ", counter);
+  counter += 1;
+}
+});
+
+const tren = createPromise();
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    // resolve('Done!');
+    return Promise.resolve({position: amountFromUser, delay: delayYxxxx});
   } else {
     // Reject
+    // reject('Error!');
+    return Promise.reject({position: amountFromUser, delay: delayYxxxx});
   }
 }
+
+// console.log(tren);
+
+tren.then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
