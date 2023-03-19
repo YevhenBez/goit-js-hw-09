0fcae76142bc009d533a7416c0684b080c5cebd1
YevhenBez <byxgalter@bigmir.net>
2023-03-19 16:16:01 +0200

    Данным куском кода я нашёл необходимые        input        и кнопку. Задание 3 Модуля 9
    Данным куском кода я нашёл необходимые        input        и кнопку. Задание 3 Модуля 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+const form = document.querySelector('.form');
+
+const inputDelay = form.firstElementChild.firstElementChild;
+
+const inputStep = form.firstElementChild.nextElementSibling.firstElementChild;
+
+const btn = form.lastElementChild;
+
+const inputAmount = btn.previousElementSibling.firstElementChild;
+
+console.log(inputAmount);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
