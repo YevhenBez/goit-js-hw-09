ea9448f1f23e2866443f177e46b8fc3085164813
YevhenBez <byxgalter@bigmir.net>
2023-03-20 01:42:39 +0200

    Данными кусками кода ввёл счётчики для    position     и    delay     (через    let positionInOrder = 1;  let delayInOrder;     ) чтобы выводить их в консоль для     position        и    delay    соответственно. Причём значение    delayInOrder    привёл к числу при помощи      delayInOrder = Number(parsedDelayStepAmount.delay);        , чтобы затем можно было сложить с      const stepFromUser    (которое также привёл к числу). Остальные функции из ТЗ Задания 3 Модуля 9  На этом данное задание полностью готово.
    Данными кусками кода ввёл счётчики для    position     и    delay     (через    let positionInOrder = 1;  let delayInOrder;     ) чтобы выводить их в консоль для     position        и    delay    соответственно. Причём значение    delayInOrder    привёл к числу при помощи      delayInOrder = Number(parsedDelayStepAmount.delay);        , чтобы затем можно было сложить с      const stepFromUser    (которое также привёл к числу). Остальные функции из ТЗ Задания 3 Модуля 9 На этом данное задание полностью готово.

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,61 +10,57 @@ const inputAmount = btn.previousElementSibling.firstElementChild;
 
 let counter = 0;
 
-let amountFromUserTren;
+let positionInOrder = 1;
+
+let delayInOrder;
 
 form.addEventListener('input', onFormInput);
 
 function onFormInput(event) {
-  // event.preventDefault();
   
   const delayStepAmount = {delay: inputDelay.value, step: inputStep.value, amount: inputAmount.value};
 
   localStorage.setItem("data-from-user", JSON.stringify(delayStepAmount)); 
 }
 
-const delayYxxxx = 300;
-
 form.addEventListener("submit", event => {
   event.preventDefault();
-  
+
   const savedDelayStepAmount = localStorage.getItem("data-from-user");
 
   const parsedDelayStepAmount = JSON.parse(savedDelayStepAmount);
 
-  console.log(parsedDelayStepAmount);
-  // {delay: '787', step: '585', amount: '44'}
-
   const amountFromUser = parsedDelayStepAmount.amount;
 
-  console.log(amountFromUser);
+  delayInOrder = Number(parsedDelayStepAmount.delay);
 
-  while (counter <= amountFromUser) {
-  console.log("counter: ", counter);
-  counter += 1;
-}
-});
+  const stepFromUser = Number(parsedDelayStepAmount.step);
 
-const tren = createPromise();
+  while (counter < amountFromUser) {
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    // resolve('Done!');
-    return Promise.resolve({position: amountFromUserTren, delay: delayYxxxx});
-  } else {
-    // Reject
-    // reject('Error!');
-    return Promise.reject({position: amountFromUserTren, delay: delayYxxxx});
-  }
-}
+    const promise = createPromise();
 
-// console.log(tren);
-
-tren.then(({ position, delay }) => {
+    promise.then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+      
+    counter += 1;
+    
+    positionInOrder += 1;
 
+    delayInOrder += stepFromUser;
+  }
+}
+);
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    return Promise.resolve({position: positionInOrder, delay: delayInOrder});
+  } else {
+    return Promise.reject({position: positionInOrder, delay: delayInOrder});
+  }
+}
