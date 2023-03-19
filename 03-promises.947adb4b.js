dc77f49c8a94356a3d4ea17cf53529d6a828ec5a
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-03-19 19:48:55 +0000

    Deploying to gh-pages from @ YevhenBez/goit-js-hw-09@1ebd88e8412624a7fa099f70f5baa114b40f9089 🚀

--- 03-promises.947adb4b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),l=(e.firstElementChild.firstElementChild,e.firstElementChild.nextElementSibling.firstElementChild,e.lastElementChild.previousElementSibling.firstElementChild);console.log(l)}();
-//# sourceMappingURL=03-promises.947adb4b.js.map
